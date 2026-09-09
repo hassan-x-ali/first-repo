c5b0fae90157c682a2be27dbbfcf70219b06d636
hassan-x-ali <a.ali.hassanx@gmail.com>
2026-09-09 17:00:14 +0500

    created file: pattrens.cpp

---
+++ pattrens.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout << "Enter number of rows in pattern: " << endl;
+    cin >> n;
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            sum += j;
+        }
+    }
+    for()
+}
