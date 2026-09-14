65dcd879d9e578dba05c7514c44016c827856d8c
hassan-x-ali <a.ali.hassanx@gmail.com>
2026-09-14 21:01:49 +0500

    added file: BitWiseOperator

---
+++ BitWiseOperators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    cout << "Bitwise AND: " << (a & b) << endl;
+    cout << "Bitwise OR: " << (a | b) << endl;
+    cout << "Bitwise XOR: " << (a ^ b) << endl;
+return 0;
+}
