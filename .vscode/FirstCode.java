ac11dd3cd89d066fd2323d5cb7f87b247a08cbd8
hassan-x-ali <a.ali.hassanx@gmail.com>
2026-09-16 13:09:46 +0500

    solve some errors in java file

--- .vscode/FirstCode.java
+++ .vscode/FirstCode.java
@@ -1,7 +1,5 @@
 class FirstCode {
-
-     public static void main(String[] args) {
-            System.out.println("Hello World");
+    public static void main(String[] args) {
+        System.out.println("Hello World");
     }
 }
-
