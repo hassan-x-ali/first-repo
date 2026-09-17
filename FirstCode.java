75fadac45aac18e8a0622e30a9f8ca8479ae17e8
hassan-x-ali <a.ali.hassanx@gmail.com>
2026-09-17 09:22:00 +0500

    solve problem in java file

--- FirstCode.java
+++ FirstCode.java
@@ -1,7 +1,9 @@
 public class FirstCode {
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
     
+       System.out.println("Hello World");
+    }
+    
+}
 
